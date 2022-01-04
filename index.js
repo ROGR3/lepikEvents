e533d1cdf45c789750354da1bbdffed2f90b460e
borecjeborec1 <robingrundel@seznam.cz>
2022-01-04 22:08:00 +0100

    linux

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const EventEmitter2 = require('eventemitter2').EventEmitter2;
 const { spawn } = require('child_process');
 
+
+
 const events = new EventEmitter2({ wildcard: true });
-const python = process.platform === "win32" ? spawn(`${__dirname + '/lib/lepikEvents.exe'}`) : spawn("sudo", [`${__dirname + '/lib/main'}`]);
+const python = process.platform === "win32" ? spawn(`${__dirname + '/build/winpy.exe'}`) : spawn("sudo", ["python", `${__dirname + '/lib/unixpy.py'}`]);
 
 python.stdout.on('data', function (data) {
   data = data.toString().split(/\r\n|\r|\n/).filter(ar => { return ar })
