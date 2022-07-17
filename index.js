4303c4115bf7ff57c0e8ab2115f46336c58e9279
borecjeborec1 <robingrundel@seznam.cz>
2022-07-17 21:49:00 +0200

    whitespace

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const EventEmitter2 = require('eventemitter2').EventEmitter2;
 const { spawn } = require('child_process');
 
-
-
 const events = new EventEmitter2({ wildcard: true });
 const python = process.platform === "win32" ? spawn(`${__dirname + '/build/winpy.exe'}`) : spawn("sudo", ["python", `${__dirname + '/lib/unixpy.py'}`]);
 
